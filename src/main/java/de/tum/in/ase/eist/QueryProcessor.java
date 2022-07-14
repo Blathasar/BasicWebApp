219d3357bfa6f6fd6cb1423cd4692b7ca4067a76
Blathasar <etjbkaspar@gmail.com>
2022-07-14 15:28:04 +0200

    largest number

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,8 +11,20 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "Elias";
+        } else if (query.contains("which of the following numbers is the largest:")) {
+            String substring = query.substring(48);
+            String[] stringNumbers = substring.split(" ");
+            int[] numbers = new int[stringNumbers.length];
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = Integer.getInteger(String.valueOf(numbers[i]));
+            }
+            int largestNumber = numbers[0];
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] > largestNumber) {
+                    largestNumber = numbers[i];
+                }
+            }
+            return "" + largestNumber;
         } else { // TODO extend the programm here
             return "";
         }
