a34c104812ad3ae9f54a5485e5d00943689afe12
Blathasar <etjbkaspar@gmail.com>
2022-07-14 15:05:19 +0200

    what is your name

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -7,12 +7,14 @@ public class QueryProcessor {
 
     public String process(String query) {
 		query = query.toLowerCase();
-        if (query.contains("Elias")) {
-            return "Hi Elias!";
+        if (query.contains("shakespeare")) {
+            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                    "English poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
-            return "";
+        } else {
+            return "that is not the name we are looking for";
         }
     }
 }
